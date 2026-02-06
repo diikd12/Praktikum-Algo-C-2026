f214603acb008d6a6582e87a72921c862c1fd0de
diikd12 <hadifauzan255@gmail.com>
2026-02-06 15:07:31 +0700

    adding Pertemuan_2

--- HadiFauzan_25071207083_TI-C/Pertemuan_2/LatihanPraktikum/function.py
+++ HadiFauzan_25071207083_TI-C/Pertemuan_2/LatihanPraktikum/function.py
@@ -3,11 +3,12 @@ for x in range (10, 90, 2):
 
 print ('done om ini akhirnya =', x)
 
-#fuunction itu adalah sebuah blok kode yang dapat digunakan kembali
-#secara bahasa mudahnya function itu adalah sebuah prosedur
-#fungsi adalah sebuah blok kode yang hanya akan berjalan ketika dipanggil
-
-def sapa():
-    print ('Halo mamang')
+def sapa(name="hadi"):
+    print ('Halo mamang', name)
 
 sapa()
+
+
+#function itu adalah sebuah blok kode yang dapat digunakan kembali
+#secara bahasa mudahnya function itu adalah sebuah prosedur
+#fungsi adalah sebuah blok kode yang hanya akan berjalan ketika dipanggil
