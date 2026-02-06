05a60596e4daa9460dba2c4ddd57a9edd1b14fab
diikd12 <hadifauzan255@gmail.com>
2026-02-06 15:22:07 +0700

    adding Pertemuan_2

--- HadiFauzan_25071207083_TI-C/Pertemuan_2/LatihanPraktikum/function.py
+++ HadiFauzan_25071207083_TI-C/Pertemuan_2/LatihanPraktikum/function.py
@@ -7,10 +7,15 @@ def sapa(name="hadi"):
     print ('Halo mamang', name)
 
 sapa()
-
 #ini namanya void function^^
 
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
 
+myfunc()
 
 #function itu adalah sebuah blok kode yang dapat digunakan kembali
 #secara bahasa mudahnya function itu adalah sebuah prosedur
