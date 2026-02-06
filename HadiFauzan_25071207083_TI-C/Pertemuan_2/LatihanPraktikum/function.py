edcf4550d4217814155fa468120ab7538407e84d
diikd12 <hadifauzan255@gmail.com>
2026-02-06 15:26:09 +0700

    adding Pertemuan_2

--- HadiFauzan_25071207083_TI-C/Pertemuan_2/LatihanPraktikum/function.py
+++ HadiFauzan_25071207083_TI-C/Pertemuan_2/LatihanPraktikum/function.py
@@ -17,6 +17,18 @@ def myfunc():
 
 myfunc()
 
+
+#recursion
+def factorial(n):
+  # Base case
+  if n == 0 or n == 1:
+    return 1
+  # Recursive case
+  else:
+    return n * factorial(n - 1)
+
+print(factorial(5))
+
 #function itu adalah sebuah blok kode yang dapat digunakan kembali
 #secara bahasa mudahnya function itu adalah sebuah prosedur
 #fungsi adalah sebuah blok kode yang hanya akan berjalan ketika dipanggil
